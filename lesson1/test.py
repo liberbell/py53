a7719c78580098f5b375a2098736cccdde1dd9e0
Liberbell <liberbell@gmail.com>
2025-11-18 22:17:19 +0900

    test4

--- lesson1/test.py
+++ lesson1/test.py
@@ -2,8 +2,7 @@ import sympy
 import pandas as pd
 p = list(sympy.primerange(1, 100))
 p_num = len(p)
-print(len(p))
 
-list1 = list(range(1, p_num + 1))
-sp = pd.Series(p, index=list1)
+idx = list(range(1, p_num + 1))
+sp = pd.Series(p, index=idx)
 print(sp)
