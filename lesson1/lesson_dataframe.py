2e5975275de4c09e3400bfbe36bd749ede5a4177
Liberbell <liberbell@gmail.com>
2025-11-23 12:03:29 +0900

    lesson_dataframe3

--- lesson1/lesson_dataframe.py
+++ lesson1/lesson_dataframe.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 list1 = [[1, 1, 1], [2, 4, 8], [3, 9, 27]]
-for x in list1:
-    print(x)
+# for x in list1:
+#     print(x)
 
-df = pd.DataFrame(list1, columns=['linear', 'square', 'cubic'])
+df = pd.DataFrame(list1, columns=['linear', 'square', 'cubic'], index=['d1', 'd2', 'd3'])
 print(df)
