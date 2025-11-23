d1492580dfabe0c0541f867aa0fdb38635348901
Liberbell <liberbell@gmail.com>
2025-11-23 12:54:30 +0900

    lesson_dataframe12

--- lesson1/lesson_dataframe.py
+++ lesson1/lesson_dataframe.py
@@ -36,3 +36,9 @@ df2.at["d3", "linear"] = 3
 df2.at["d3", "square"] = 9
 df2.at["d3", "cubic"] = 27
 print(df2)
+
+df3 = pd.DataFrame()
+df3.at["d1", "linear"] = 1
+df3.at["d2", "square"] = 4
+df3.at["d3", "cubic"] = 27
+print(df3)
