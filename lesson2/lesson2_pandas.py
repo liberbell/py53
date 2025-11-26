b7aafe2cc5c9495f1e7c01739aaa0e1608b8792e
Liberbell <liberbell@gmail.com>
2025-11-27 08:35:02 +0900

    lesson2_pandas4

--- lesson2/lesson2_pandas.py
+++ lesson2/lesson2_pandas.py
@@ -3,3 +3,7 @@ import pandas as pd
 list1 = [[1, 1, 1], [2, 4, 8], [3, 9, 27]]
 df = pd.DataFrame(list1, index=["d1", "d2", "d3"], columns=["linear", "square", "cubic"])
 print(df)
+
+dname = r"/data"
+fname = dname + r"/csv01.csv"
+print(fname)
