8194596d6ce40bef1537cf07d40f20551d1ac7ed
Liberbell <liberbell@gmail.com>
2025-11-28 09:15:22 +0900

    lesson3_statics4

--- lesson3/lesson3_statics.py
+++ lesson3/lesson3_statics.py
@@ -1,4 +1,9 @@
 import pandas as pd
 from scipy import stats
 
-print(stats.uniform.rvs(size=5, random_state=3))
+# print(stats.uniform.rvs(size=5, random_state=3))
+
+df = pd.DataFrame()
+df["Uni"] = stats.uniform.rvs(size=10000)
+df["Norm"] = stats.norm.rvs(size=10000)
+print(df)
