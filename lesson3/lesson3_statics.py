75f7f58afb92d6be141f09700a2b3a5410bc8094
Liberbell <liberbell@gmail.com>
2025-11-28 19:46:05 +0900

    lesson3_statics6

--- lesson3/lesson3_statics.py
+++ lesson3/lesson3_statics.py
@@ -6,4 +6,5 @@ from scipy import stats
 df = pd.DataFrame()
 df["Uni"] = stats.uniform.rvs(size=10000)
 df["Norm"] = stats.norm.rvs(loc=0, scale=1, size=10000)
+df["LogNorm"] = stats.lognorm.rvs(loc=0, s=1, size=10000)
 print(df)
