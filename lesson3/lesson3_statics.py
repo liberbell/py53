a2c2d6a86962e899e10abbff1052aa0fe12d6047
Liberbell <liberbell@gmail.com>
2025-11-29 10:51:12 +0900

    lesson3_statics3

--- lesson3/lesson3_statics.py
+++ lesson3/lesson3_statics.py
@@ -10,5 +10,6 @@ df["Norm"] = stats.norm.rvs(loc=0, scale=1, size=10000)
 df["LogNorm"] = stats.lognorm.rvs(loc=0, s=1, size=10000)
 print(df)
 
-plt.plot(df["Uni"])
+# plt.plot(df["Uni"])
+df["Uni"].plot()
 plt.show()
