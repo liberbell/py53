96b1c7896b789c7b08bbc810f0d172dfc06cd9e8
Liberbell <liberbell@gmail.com>
2025-11-29 10:42:28 +0900

    lesson3_statics2

--- lesson3/lesson3_statics.py
+++ lesson3/lesson3_statics.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from scipy import stats
+import matplotlib.pyplot as plt
 
 # print(stats.uniform.rvs(size=5, random_state=3))
 
@@ -9,4 +10,5 @@ df["Norm"] = stats.norm.rvs(loc=0, scale=1, size=10000)
 df["LogNorm"] = stats.lognorm.rvs(loc=0, s=1, size=10000)
 print(df)
 
-print(df["Uni"].hist())
+plt.plot(df["Uni"])
+plt.show()
