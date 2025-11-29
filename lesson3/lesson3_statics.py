6997125b3f11cdac1ef498634450bdce4af1f70a
Liberbell <liberbell@gmail.com>
2025-11-29 10:39:42 +0900

    lesson3_statics1

--- lesson3/lesson3_statics.py
+++ lesson3/lesson3_statics.py
@@ -8,3 +8,5 @@ df["Uni"] = stats.uniform.rvs(size=10000)
 df["Norm"] = stats.norm.rvs(loc=0, scale=1, size=10000)
 df["LogNorm"] = stats.lognorm.rvs(loc=0, s=1, size=10000)
 print(df)
+
+print(df["Uni"].hist())
