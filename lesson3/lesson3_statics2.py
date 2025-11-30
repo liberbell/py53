bf65d82ec950ec54d10441593cf21c1bb9633726
Liberbell <liberbell@gmail.com>
2025-11-30 23:19:32 +0900

    lesson3_statics22

--- lesson3/lesson3_statics2.py
+++ lesson3/lesson3_statics2.py
@@ -3,5 +3,7 @@ from scipy import stats
 
 df = pd.DataFrame()
 df["Norm"] = stats.norm.rvs(loc=0, scale=1, size=10000)
-
+df["LogNorm"] = stats.lognorm.rvs(loc=0, s=1, size=10000)
+df.hist(bins=20)
+df.describe()
 print(df)
