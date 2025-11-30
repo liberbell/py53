0a6953144f373a6a1a0c039d2236a499b2aff1bb
Liberbell <liberbell@gmail.com>
2025-11-30 23:33:07 +0900

    lesson3_statics25

--- lesson3/lesson3_statics2.py
+++ lesson3/lesson3_statics2.py
@@ -8,4 +8,7 @@ df["LogNorm"] = stats.lognorm.rvs(loc=0, s=1, size=10000)
 df.hist(bins=20)
 print(df.describe())
 plt.plot(df)
-plt.show()
+# plt.show()
+
+B = df["Norm"].value_counts(bins=20)
+print(B)
