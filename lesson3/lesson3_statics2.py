d47ebfd86bfb8d0110d240fc29506de32dd968d3
Liberbell <liberbell@gmail.com>
2025-11-30 23:24:36 +0900

    lesson3_statics23

--- lesson3/lesson3_statics2.py
+++ lesson3/lesson3_statics2.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from scipy import stats
+import matplotlib.pyplot as plt
 
 df = pd.DataFrame()
 df["Norm"] = stats.norm.rvs(loc=0, scale=1, size=10000)
 df["LogNorm"] = stats.lognorm.rvs(loc=0, s=1, size=10000)
 df.hist(bins=20)
-df.describe()
-print(df)
+plt.plot(df.hist())
+plt.show()
