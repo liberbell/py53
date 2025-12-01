ef66b91680731d89745cac749753b753f5e64551
Liberbell <liberbell@gmail.com>
2025-12-01 17:05:31 +0900

    lesson3_statics29

--- lesson3/lesson3_statics2.py
+++ lesson3/lesson3_statics2.py
@@ -42,6 +42,6 @@ fname = r"data/" + "testFig02.png"
 
 fname = r"data/" + "testFig03.png"
 plt.figure()
-df["Norm"].hist(bins=30)
+df["Norm"].hist(bins=30, alpha=0.6)
 df["LogNorm"].hist(bins=30)
 plt.savefig(fname)
