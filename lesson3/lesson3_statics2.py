f493200a42e540176ea9254eb6ba86a8e3f71d8c
Liberbell <liberbell@gmail.com>
2025-12-02 08:12:51 +0900

    lesson3_statics23

--- lesson3/lesson3_statics2.py
+++ lesson3/lesson3_statics2.py
@@ -44,4 +44,6 @@ fname = r"data/" + "testFig03.png"
 plt.figure()
 df["Norm"].hist(bins=30, alpha=0.6, range=(-3, 7))
 df["LogNorm"].hist(bins=30, alpha=0.6, range=(-3, 7))
+plt.xlabel("value")
+plt.ylabel("freq")
 plt.savefig(fname)
