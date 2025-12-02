d35493893862d0c28838f6557e2cbc3c53e9c492
Liberbell <liberbell@gmail.com>
2025-12-03 08:44:43 +0900

    lesson4_pivot7

--- lesson4/lesson4_pivot.py
+++ lesson4/lesson4_pivot.py
@@ -8,14 +8,15 @@ print(["a", "b", "c"] * 3)
 p = ["osaka", "kyoto", "tokyo"] * 100
 random.seed(1)
 random.shuffle(p)
-print(p)
+# print(p)
 
 random.seed(2)
 m = ["okoomi"] * 80 + ["curry"] * 120 + ["oden"] * 50 + ["udon"] * 50
 random.shuffle(m)
-print(m)
+# print(m)
 
 df = pd.DataFrame()
 df["comefrom"] = p
 df["favorite"] = m
-df["age"] = stats.norm.rvs(loc=35, scale=10, size=300, random_state=3)
+df["age"] = stats.norm.rvs(loc=35, scale=10, size=300, random_state=3).astype("int32")
+print(df)
