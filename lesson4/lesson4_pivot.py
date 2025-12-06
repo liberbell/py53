69c363bca7372bb4719f6aa1267415d8365b9f70
Liberbell <liberbell@gmail.com>
2025-12-06 10:26:08 +0900

    lesson4_pivot4

--- lesson4/lesson4_pivot.py
+++ lesson4/lesson4_pivot.py
@@ -73,6 +73,7 @@ df3["cos"] = y2
 print(df3.head(5))
 
 
-df3.plot(x="x", y=["sin", "cos"], marker="s", lw=0.8)
+df3.plot(x="x", y=["sin", "cos"], lw=0.8)
 plt.title("graph of sin and cos")
+plt.grid()
 plt.show()
