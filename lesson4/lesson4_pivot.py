b1c19a2a5ca35cc44f9f3b029cd2e75a497fe273
Liberbell <liberbell@gmail.com>
2025-12-07 10:43:41 +0900

    lesson4_pivot1

--- lesson4/lesson4_pivot.py
+++ lesson4/lesson4_pivot.py
@@ -91,7 +91,14 @@ x2 = stats.norm.rvs(loc=8, scale=2, size=800, random_state=2)
 x12 = np.append(x1, x2)
 print(len(x12))
 
-y1 = stats.norm.rvs(loc=1, scale=1, size=200, random_state=1)
-y2 = stats.norm.rvs(loc=8, scale=2, size=800, random_state=2)
+y1 = stats.norm.rvs(loc=1, scale=1, size=200, random_state=3)
+y2 = stats.norm.rvs(loc=8, scale=2, size=800, random_state=4)
 y12 = np.append(y1, y2)
 print(len(y12))
+
+df4 = pd.DataFrame()
+df4["x"] = x12
+df4["y"] = y12
+
+df4.hist(bins=20, figsize=(12, 3))
+plt.show()
